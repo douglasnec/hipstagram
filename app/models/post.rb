dc4ea9f948b298cbf5d2fa3d0e2b3ce19ddf311f
Ericat <erica.salvaneschi@hotmail.com>
2014-01-20 17:23:23 +0000

    Added more features

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ActiveRecord::Base
   has_many :comments
-  has_attached_file :image, styles: { thumb: "300x300>" }
+  has_attached_file :image, 
+                    styles: { thumb: "300x300>" },
+                    storage: :s3,
+                    s3_credentials: {
+                      access_key_id: 'AKIAJ57EWJKMPW4556TA',
+                      secret_access_key: Rails.application.secrets.s3_secret
+                    },
+                    bucket: 'hipstagram_development'
 end
